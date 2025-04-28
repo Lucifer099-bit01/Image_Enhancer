290841d1d1b828df20f305dd7f2620e2c6fc0589
Lucifer099-bit01 <rs7729808@gmail.com>
2025-04-28 18:52:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ const App = () => {
     <div className="flex flex-col items-center justify-center  bg-black min-h-screen w-full text-white p-2">
       <div className="text-center mb-8">
         <h1 className="text-5xl text-bold outline-text mb-5">AI Image Enhancer</h1>
-        <p className="text-gray-400">Upload Your Image And Let AI Do It For You</p>
+        <p className="text-gray-400">Upload Your Image And Let AI Do It For You </p>
       </div>
       <Home />
-     <div className="text-sm text-gray-500 mt-8">Powered by PicWish API</div>
+     <div className="text-sm text-gray-500 mt-8">Powered by PicWish API </div>
     </div>
   );
 };
